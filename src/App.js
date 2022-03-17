02b7d4fd0637c0342b5add8fbaca691c703ed2ee
FlyLeBro <73607908+FlyLeBro@users.noreply.github.com>
2022-03-17 13:09:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
+  const [todos, setToDos] = React.useState([])
+  const [todo, setToDo] = React.useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false
+    }
+
+    setToDos([...todos].concat(newTodo))
+    setToDo('')
+  }
+
+  function deleteTodo(id) {
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+    setToDos(updatedTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setToDo(e.target.value)} value={todo}/>
+        <button className='btn btn-success' type="submit">Ajouter</button>
+      </form>
+      {todos.map((todo) => <div className='mt-4' key={todo.id}>
+        {todo.text}
+        <button className='btn btn-danger' onClick={() => deleteTodo(todo.id)}>Supprimer</button>
+        </div>)}
     </div>
   );
 }
